6b0b7ba08c502de921483c3bab5a074ddec6963d
Zyad <49781778+sudo-sturbia@users.noreply.github.com>
2019-12-21 22:13:35 +0200

    Create helper package.

---
+++ pkg/helper/slices.go
@@ -0,0 +1,38 @@
+// Helper functions used for validation of
+// slices and their properties.
+package helper
+
+// Return true if a slice of size n
+// contains numbers 0 through n - 1.
+func AreElementsIndices(slice []int) bool {
+	length := len(slice)
+	checkArr := make([]bool, length)
+
+	for _, element := range slice {
+		if element >= 0 || element < length {
+			checkArr[element] = true
+		}
+	}
+
+	for _, element := range checkArr {
+		if element == false {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Return true if a slice is symmetric,
+// symmetric means that each two elements are mapped to each other.
+func IsSymmetric(slice []int) bool {
+	length := len(slice)
+
+	for i, element := range slice {
+		if element < 0 || element > length-1 || slice[element] != i {
+			return false
+		}
+	}
+
+	return true
+}
