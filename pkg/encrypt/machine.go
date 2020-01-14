60e59f35e5b000f5f1be536b99974637d093941d
Zyad <49781778+sudo-sturbia@users.noreply.github.com>
2020-01-14 13:17:51 +0200

    Created New method to create Machine objects.

--- pkg/encrypt/machine.go
+++ pkg/encrypt/machine.go
@@ -2,6 +2,11 @@
 // Used for encryption and decryption of messages.
 package encrypt
 
+import (
+	"fmt"
+	"os"
+)
+
 const (
 	numberOfRotors = 3
 	alphabetSize   = 26
@@ -17,6 +22,28 @@ type Machine struct {
 	takenSteps [numberOfRotors - 1]int           // Number of steps taken by each rotor except the last
 	step       int                               // Size of shift between rotor steps (move)
 	cycle      int                               // Number of steps considered a full cycle, considered by following rotor when stepping
+}
+
+// New returns a newly created, and initialized Machine object.
+// Machine's fields are read from config file ~/.config/enigma.json
+// If file's configs are correct the machine is initialized and returned
+// and error is `nil`.
+// Otherwise `write` parameter is checked. If write is true, random configs
+// are generated and written to file, machine is returned and error is `nil`.
+// Otherwise `nil` and an initError are returned.
+func New(write bool) (*Machine, error) {
+	machine, err := load(os.Getenv("HOME") + "/.config/enigma.json")
+
+	if err != nil {
+		if write {
+			// TODO
+			return nil, nil
+		}
+
+		return nil, &initError{fmt.Sprintf("configuration error: %v", err.Error())}
+	}
+
+	return machine, nil
 }
 
 // PathConnections returns electric pathway connections
