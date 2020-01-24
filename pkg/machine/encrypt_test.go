f42632d87a976154373247e59f651e75c45553e9
Zyad <49781778+sudo-sturbia@users.noreply.github.com>
2020-01-24 21:02:24 +0200

    Re-arranged tests based on character types. Created more non-alphabetical cases.

--- pkg/machine/encrypt_test.go
+++ pkg/machine/encrypt_test.go
@@ -4,48 +4,44 @@ import (
 	"testing"
 )
 
-// Test character encryption using config 1.
-func TestEncryptCharConfig1(t *testing.T) {
-	m, err := read("../../test/data/config-1.json")
+// Test encryption of individual alphabetical characters.
+func TestEncryptCharAlpha(t *testing.T) {
+	// Using configuration 1
+	m1, err := read("../../test/data/config-1.json")
 	if err != nil {
 		t.Errorf("could not read configurations\n%s", err.Error())
 	}
 
 	// r -> u
-	encrypted := m.encryptChar('r')
-	if encrypted != 'u' {
-		t.Errorf("character 'r' encrypted to '%c', expected 'u'", encrypted)
+	encrypted1 := m1.encryptChar('r')
+	if encrypted1 != 'u' {
+		t.Errorf("character 'r' encrypted to '%c', expected 'u'", encrypted1)
 	}
 
-	// , -> ,
-	encrypted = m.encryptChar(',')
-	if encrypted != ',' {
-		t.Errorf("character ',' encrypted to '%c', expected ','", encrypted)
-	}
-
-	// ' ' -> ' '
-	encrypted = m.encryptChar(' ')
-	if encrypted != ' ' {
-		t.Errorf("character ' ' encrypted to '%c', expected ' '", encrypted)
-	}
-
-	// 1 -> 1
-	encrypted = m.encryptChar('1')
-	if encrypted != '1' {
-		t.Errorf("character '1' encrypted to '%c', expected '1'", encrypted)
-	}
-}
-
-// Test character encryption using config 2.
-func TestEncryptCharConfig2(t *testing.T) {
-	m, err := read("../../test/data/config-2.json")
+	// Using configuration 2
+	m2, err := read("../../test/data/config-2.json")
 	if err != nil {
 		t.Errorf("could not read configurations\n%s", err.Error())
 	}
 
 	// s -> d
-	encrypted := m.encryptChar('s')
-	if encrypted != 'd' {
-		t.Errorf("character 's' encrypted to '%c', expected 'd'", encrypted)
+	encrypted2 := m2.encryptChar('s')
+	if encrypted2 != 'd' {
+		t.Errorf("character 's' encrypted to '%c', expected 'd'", encrypted2)
+	}
+
+}
+
+// Test encryption of a list of non-alphabetical characters.
+// Characters are not meant to change when encrypted.
+func TestEncryptCharNonAlpha(t *testing.T) {
+	m := Generate()
+
+	nonAlpha := []byte{',', ' ', '1', '\n', '[', '\t'}
+	for _, char := range nonAlpha {
+		encrypted := m.encryptChar(char)
+		if encrypted != char {
+			t.Errorf("character '%c' encrypted to '%c', expected '%c'", char, encrypted, char)
+		}
 	}
 }
