8567b3cae5a7b9a625ea3905c09cc49f7563c60e
Zyad <49781778+sudo-sturbia@users.noreply.github.com>
2020-02-02 00:10:51 +0200

    Patch-2.0: refactored Encrypt method.
    - Allows for correct decryption (connections are symmetric).
    - Employs changed rotors.

--- pkg/machine/encrypt.go
+++ pkg/machine/encrypt.go
@@ -8,8 +8,9 @@ import (
 
 // Encrypt encrypts a string using a Machine object.
 // Returns encrypted string and an error in case of an incorrect configuration.
-// Non-alphabetical characters are returned without change, and don't affect
-// rotors' movement (rotors are not shifted).
+// Uppercase and lowercase letters are treated similarly and produce the same
+// results. Non-alphabetical characters are returned without change, and don't
+// affect rotors' movement (rotors are not shifted).
 func (m *Machine) Encrypt(message string) (string, error) {
 	if err := m.isInit(); err != nil {
 		return "", err
@@ -33,12 +34,15 @@ func (m *Machine) encryptChar(char byte) byte {
 
 	encryptedChar := m.plugIn(char)
 	for i := 0; i < m.numberOfRotors; i++ {
-		encryptedChar = m.pathConnections[i][m.rotors[i][encryptedChar]]
+		index := (encryptedChar + m.rotors[i]) % alphabetSize
+		encryptedChar = m.pathConnections[i][index]
 	}
 
 	encryptedChar = m.reflector[encryptedChar]
-	for i := 0; i < m.numberOfRotors; i++ {
-		encryptedChar = m.rotors[m.numberOfRotors-i-1][m.pathConnections[m.numberOfRotors-i-1][encryptedChar]]
+
+	flipped := m.flippedConnections()
+	for i := m.numberOfRotors - 1; i >= 0; i-- {
+		encryptedChar = (flipped[i][encryptedChar] - m.rotors[i] + alphabetSize) % alphabetSize
 	}
 
 	m.stepRotors()
@@ -56,4 +60,17 @@ func (m *Machine) plugIn(char byte) int {
 // plugboard connections. Used when character is returned.
 func (m *Machine) plugOut(char int) byte {
 	return byte(m.plugboardConnections[char] + 'a')
+}
+
+// flippedConnections returns a slice of flipped pathway connections
+// to be used in encryption cycle after reflecting.
+func (m *Machine) flippedConnections() [][alphabetSize]int {
+	flipped := make([][alphabetSize]int, m.numberOfRotors)
+	for i, slice := range m.pathConnections {
+		for j, val := range slice {
+			flipped[i][val] = j
+		}
+	}
+
+	return flipped
 }
