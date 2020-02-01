f4af40987273b66638e606cc7ab43d9eb2137a4b
Zyad <49781778+sudo-sturbia@users.noreply.github.com>
2020-02-02 00:45:25 +0200

    Updated package's documentation to explain configuration options more thoroughly.

--- pkg/machine/machine.go
+++ pkg/machine/machine.go
@@ -3,23 +3,59 @@ Package machine represents an enigma machine used for encryption and
 decryption of text messages.
 
 The machine consists of the following components: electric pathways,
-reflector, plugboard, and a variable number rotors. All of which can
-be configured through JSON or a collective setter.
+reflector, plugboard, and a variable number of rotors.
 
-A machine can be created using machine.Load, machine.Generate, machine.Read,
-or by simply creating a Machine object and calling SetComponents method.
+A machine object can be created using machine.Load, machine.Generate,
+machine.Read, or by simply creating a empty Machine object and calling
+SetComponents method.
 
 machine.Load is meant for usage in an independent program. The other
 three options are more suitable for usage when the package is imported.
 
-A machine can have any number of rotors given that it's > 0. Other
-properties related to rotors such as step and cycle sizes can also be
-configured.
-
-Encrypt method is used to encrypt (or decrypt) a given message as
-the following example
+Encrypt is the method used for encryption (or decryption) of strings
+and is simply used as the following.
     m := machine.Generate(3)
     encrypted := m.Encrypt("message")
+
+Configuration
+
+All components of the machine can be configured through JSON or using the
+collective setter SetComponents. An example of a JSON config file is the
+following
+	{
+		"pathways": [
+			["a", "b", "c", ...],
+			["a", "b", "c", ...],
+			["a", "b", "c", ...]
+		],
+		"reflector": ["a", "b", "c", ...],
+		"plugboard": ["a", "b", "c", ...],
+		"rotorPositions": ["a", "b", "c"],
+		"rotorStep": 1,
+		"rotorCycle": 26
+	}
+
+Connections: pathways, reflector, and plugboard, are all specified through
+JSON arrays where elements' indices represent a character's position in
+the alphabet. Meaning that if, in reflector array, element at index 0 is
+"b" then "a" is connected to "b".
+
+A machine can have any number of rotors given that it's > 0. A machine's
+step and cycle sizes can also be configured.
+
+Step represents the size of the shift between rotor steps. For example if
+step size is 2 then rotors jump 2 positions when shifting. A rotor at position
+"a" jumps to "c".
+
+Cycle represents the number of steps considered a full cycle, after which
+the following rotor is shifted. For example in a 3-rotor machine if cycle
+size is 13 then the second rotor is shifted once every time the first rotor
+completes 13 steps, the third rotor operates similarly but depends on second
+rotor's movement, etc.
+
+To avoid collisions and guarantee that a rotor configuration can be reached
+using only one step sequence (step x cycle) must divide 26 (alphabet size).
+Combinations that don't satisfy that relation are considered wrong.
 
 Licensed under MIT license @github.com/sudo-sturbia
 */
