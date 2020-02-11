02633067a35cda2148c52c21387ca12f12aea477
Zyad <49781778+sudo-sturbia@users.noreply.github.com>
2020-02-11 23:59:17 +0200

    Refactored verification methods in verify.go

--- pkg/machine/verify.go
+++ pkg/machine/verify.go
@@ -17,59 +17,26 @@ func (err *initError) Error() string {
 // initialized correctly, returns an error if not.
 func (m *Machine) IsConfigCorrect() error {
 	switch {
-	case len(m.pathConnections) != m.numberOfRotors:
-		return &initError{"invalid number of pathway layers"}
-	case len(m.rotors) != m.numberOfRotors:
-		return &initError{"invalid number of rotors"}
-	case !m.areRotorsInit():
-		return &initError{"invalid rotor configs"}
-	case !m.arePathwaysInit():
-		return &initError{"invalid pathway connections"}
-	case !m.isReflectorInit():
-		return &initError{"invalid reflector connections"}
-	case !m.isPlugboardInit():
-		return &initError{"invalid plugboard connections"}
-	case m.step <= 0:
-		return &initError{"invalid step size"}
-	case m.cycle <= 0:
-		return &initError{"invalid cycle size"}
+	case !m.areRotorsCorrect():
+		return &initError{"rotors configurations are invalid"}
+	case !m.isReflectorCorrect():
+		return &initError{"reflector connections are incorrect"}
+	case !m.isPlugboardCorrect():
+		return &initError{"plugboard connections are incorrect"}
 	default:
 		return nil
 	}
 }
 
-// areRotorsInit returns true if rotors are initialized correctly and
+// areRotorsCorrect returns true if rotors are initialized correctly and
 // verifies all rotor related values.
-func (m *Machine) areRotorsInit() bool {
-	if m.rotors == nil || m.takenSteps == nil {
+func (m *Machine) areRotorsCorrect() bool {
+	if m.rotors == nil || len(m.rotors) == 0 || m.numberOfRotors != len(m.rotors) {
 		return false
 	}
 
-	for _, rotorHead := range m.rotors {
-		if rotorHead < 0 || rotorHead >= alphabetSize {
-			return false
-		}
-	}
-
-	if err := m.areStepCycleValid(m.step, m.cycle); err != nil {
-		return false
-	}
-
-	if err := m.arePositionsValid(m.RotorPositions()); err != nil {
-		return false
-	}
-
-	return true
-}
-
-// arePathwaysInit returns true if pathways are initialized correctly.
-func (m *Machine) arePathwaysInit() bool {
-	if m.pathConnections == nil {
-		return false
-	}
-
-	for _, pathwaysArr := range m.pathConnections {
-		if !helper.AreElementsIndices(pathwaysArr[:]) {
+	for _, rotor := range m.rotors {
+		if err := rotor.IsConfigCorrect(); err != nil {
 			return false
 		}
 	}
@@ -77,14 +44,14 @@ func (m *Machine) arePathwaysInit() bool {
 	return true
 }
 
-// isReflectorInit returns true if reflector is initialized correctly.
-func (m *Machine) isReflectorInit() bool {
+// isReflectorCorrect returns true if reflector is initialized correctly.
+func (m *Machine) isReflectorCorrect() bool {
 	return helper.AreElementsIndices(m.reflector[:]) &&
 		helper.IsSymmetric(m.reflector[:])
 }
 
-// isPlugboardInit returns true if plugboard connections initialized correctly.
-func (m *Machine) isPlugboardInit() bool {
-	return helper.AreElementsIndices(m.plugboardConnections[:]) &&
-		helper.IsSymmetric(m.plugboardConnections[:])
+// isPlugboardCorrect returns true if plugboard connections initialized correctly.
+func (m *Machine) isPlugboardCorrect() bool {
+	return helper.AreElementsIndices(m.plugboard[:]) &&
+		helper.IsSymmetric(m.plugboard[:])
 }
